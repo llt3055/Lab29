7f312e41599fcaa0a5aa798d9b7ff1746c0933ec
llt3055 <llt30555@gmail.com>
2026-04-16 12:09:35 +0800

    add sth more

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Include necessary headers for file handling, data structures, etc.
 #include <iostream>
 #include <map>
 #include <array>
@@ -7,3 +8,26 @@
 #include <sstream>
 
 using namespace std;
+
+// Initialize a map to store department information, each associated with an array of lists for enrolled, waitlisted, and dropped students
+typedef map<string, array<list<string>, 3>> RegistrationMap;
+
+class RegistrationSimulation {
+    private:
+    public:
+}
+
+void loadData(string& filename) {
+
+}
+
+
+// Define main function
+int main() {
+    RegistrationSimulation sim; 
+    sim.loadData("students_data.txt");
+    sim.runSimulation();
+
+    // End of main function
+    return 0;
+}
